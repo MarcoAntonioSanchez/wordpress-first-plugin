94e347b1a850a6530e5de6ce1b49c4ccb56230fc
MarcoAntonioSanchez <marco.a.sanchez.dev@gmail.com>
2024-07-22 16:06:54 -0600

    02. Plugin base directory and file allong with the plugin's development data

---
+++ wordpress-first-plugin.php
@@ -0,0 +1,11 @@
+<?php
+/*
+    Plugin Name:    WordPress First Plugin
+    Plugin URI:     http://ravenmilosh.com/wordpress-first-plugin
+    Description:    WordPress first plugin development practice
+    Author:         Raven D. Milosh
+    Author URI:     https://ravenmilosh.com
+    Version:        0.1.0
+    Text Domain:    ravenmilosh
+    License:        GPLv2
+*/
