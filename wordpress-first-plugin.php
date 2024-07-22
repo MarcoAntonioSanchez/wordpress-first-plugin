b0e8f22f18eb76b026fbe75ee7f782afd0e45fc2
MarcoAntonioSanchez <marco.a.sanchez.dev@gmail.com>
2024-07-22 16:30:20 -0600

    03. JS and CSS enqueue with wordpress add_action

--- wordpress-first-plugin.php
+++ wordpress-first-plugin.php
@@ -9,3 +9,18 @@
     Text Domain:    ravenmilosh
     License:        GPLv2
 */
+
+/******************************************************************
+ Enqueue scripts and styles
+******************************************************************/
+
+function first_plugin_enqueue_scripts_styles() {
+
+    wp_register_style('first_plugin_style', plugins_url('styles/style.css', __FILE__));
+    wp_enqueue_style('first_plugin_style');
+
+    wp_register_script('first_plugin_script', plugins_url('scripts/my-script.js', __FILE__));
+    wp_enqueue_script('first_plugin_script');
+
+}
+add_action('wp_enqueue_scripts', 'first_plugin_enqueue_scripts_styles');
